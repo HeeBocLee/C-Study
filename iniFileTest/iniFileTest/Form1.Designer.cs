fef6228547bddd40f938683880dc196b722b93c6
SuitLee <11496013@qq.com>
2025-06-09 02:12:24 +0800

    처음 만든 ini 파일 Read/Wirte 프로그램

---
+++ iniFileTest/iniFileTest/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace iniFileTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            btnLogIn = new Button();
+            btnLogOff = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            tBoxFruit_001 = new TextBox();
+            tBoxFruit_002 = new TextBox();
+            tBoxFruit_003 = new TextBox();
+            tBoxCar_003 = new TextBox();
+            tBoxCar_002 = new TextBox();
+            tBoxCar_001 = new TextBox();
+            btnFruitSave = new Button();
+            btnCarSave = new Button();
+            btnWriteCSV = new Button();
+            btnReadCSV = new Button();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(127, 80);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(232, 28);
+            comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(127, 123);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(232, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // btnLogIn
+            // 
+            btnLogIn.Location = new Point(127, 177);
+            btnLogIn.Name = "btnLogIn";
+            btnLogIn.Size = new Size(94, 29);
+            btnLogIn.TabIndex = 2;
+            btnLogIn.Text = "登录";
+            btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // btnLogOff
+            // 
+            btnLogOff.Location = new Point(265, 177);
+            btnLogOff.Name = "btnLogOff";
+            btnLogOff.Size = new Size(94, 29);
+            btnLogOff.TabIndex = 3;
+            btnLogOff.Text = "注册";
+            btnLogOff.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(391, 123);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "修改";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(509, 123);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "保存";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(127, 240);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Section - Fruit";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(127, 378);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Section - Car";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(127, 276);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Key - 001";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(127, 307);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Key - 002";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(127, 338);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 20);
+            label5.TabIndex = 10;
+            label5.Text = "Key - 003";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(127, 476);
+            label6.Name = "label6";
+            label6.Size = new Size(77, 20);
+            label6.TabIndex = 13;
+            label6.Text = "Key - 003";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(127, 445);
+            label7.Name = "label7";
+            label7.Size = new Size(77, 20);
+            label7.TabIndex = 12;
+            label7.Text = "Key - 002";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(127, 414);
+            label8.Name = "label8";
+            label8.Size = new Size(77, 20);
+            label8.TabIndex = 11;
+            label8.Text = "Key - 001";
+            // 
+            // tBoxFruit_001
+            // 
+            tBoxFruit_001.Location = new Point(234, 273);
+            tBoxFruit_001.Name = "tBoxFruit_001";
+            tBoxFruit_001.Size = new Size(125, 27);
+            tBoxFruit_001.TabIndex = 14;
+            // 
+            // tBoxFruit_002
+            // 
+            tBoxFruit_002.Location = new Point(234, 304);
+            tBoxFruit_002.Name = "tBoxFruit_002";
+            tBoxFruit_002.Size = new Size(125, 27);
+            tBoxFruit_002.TabIndex = 15;
+            // 
+            // tBoxFruit_003
+            // 
+            tBoxFruit_003.Location = new Point(234, 335);
+            tBoxFruit_003.Name = "tBoxFruit_003";
+            tBoxFruit_003.Size = new Size(125, 27);
+            tBoxFruit_003.TabIndex = 16;
+            // 
+            // tBoxCar_003
+            // 
+            tBoxCar_003.Location = new Point(234, 473);
+            tBoxCar_003.Name = "tBoxCar_003";
+            tBoxCar_003.Size = new Size(125, 27);
+            tBoxCar_003.TabIndex = 19;
+            // 
+            // tBoxCar_002
+            // 
+            tBoxCar_002.Location = new Point(234, 442);
+            tBoxCar_002.Name = "tBoxCar_002";
+            tBoxCar_002.Size = new Size(125, 27);
+            tBoxCar_002.TabIndex = 18;
+            // 
+            // tBoxCar_001
+            // 
+            tBoxCar_001.Location = new Point(234, 411);
+            tBoxCar_001.Name = "tBoxCar_001";
+            tBoxCar_001.Size = new Size(125, 27);
+            tBoxCar_001.TabIndex = 17;
+            // 
+            // btnFruitSave
+            // 
+            btnFruitSave.Location = new Point(265, 238);
+            btnFruitSave.Name = "btnFruitSave";
+            btnFruitSave.Size = new Size(94, 29);
+            btnFruitSave.TabIndex = 20;
+            btnFruitSave.Text = "保存";
+            btnFruitSave.UseVisualStyleBackColor = true;
+            btnFruitSave.Click += btnFruitSave_Click;
+            // 
+            // btnCarSave
+            // 
+            btnCarSave.Location = new Point(265, 374);
+            btnCarSave.Name = "btnCarSave";
+            btnCarSave.Size = new Size(94, 29);
+            btnCarSave.TabIndex = 21;
+            btnCarSave.Text = "保存";
+            btnCarSave.UseVisualStyleBackColor = true;
+            btnCarSave.Click += btnCarSave_Click;
+            // 
+            // btnWriteCSV
+            // 
+            btnWriteCSV.Location = new Point(521, 240);
+            btnWriteCSV.Name = "btnWriteCSV";
+            btnWriteCSV.Size = new Size(94, 29);
+            btnWriteCSV.TabIndex = 22;
+            btnWriteCSV.Text = "写入CSV";
+            btnWriteCSV.UseVisualStyleBackColor = true;
+            btnWriteCSV.Click += btnWriteCSV_Click;
+            // 
+            // btnReadCSV
+            // 
+            btnReadCSV.Location = new Point(654, 240);
+            btnReadCSV.Name = "btnReadCSV";
+            btnReadCSV.Size = new Size(94, 29);
+            btnReadCSV.TabIndex = 23;
+            btnReadCSV.Text = "读取CSV";
+            btnReadCSV.UseVisualStyleBackColor = true;
+            btnReadCSV.Click += btnReadCSV_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1139, 668);
+            Controls.Add(btnReadCSV);
+            Controls.Add(btnWriteCSV);
+            Controls.Add(btnCarSave);
+            Controls.Add(btnFruitSave);
+            Controls.Add(tBoxCar_003);
+            Controls.Add(tBoxCar_002);
+            Controls.Add(tBoxCar_001);
+            Controls.Add(tBoxFruit_003);
+            Controls.Add(tBoxFruit_002);
+            Controls.Add(tBoxFruit_001);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(btnLogOff);
+            Controls.Add(btnLogIn);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Button btnLogIn;
+        private Button btnLogOff;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox tBoxFruit_001;
+        private TextBox tBoxFruit_002;
+        private TextBox tBoxFruit_003;
+        private TextBox tBoxCar_003;
+        private TextBox tBoxCar_002;
+        private TextBox tBoxCar_001;
+        private Button btnFruitSave;
+        private Button btnCarSave;
+        private Button btnWriteCSV;
+        private Button btnReadCSV;
+    }
+}
---
+++ iniFileTest/iniFileTest/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System.Data;
+using System.IO;
+using System.Text;
+
+using System.Runtime.InteropServices;
+using static System.Collections.Specialized.BitVector32;
+using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace iniFileTest
+{
+    public partial class Form1 : Form
+    {
+        private string _striniPath = @"D:\\iniFileTest.ini";
+        string _readFile = @"D:\\csvFileTest.csv";
+        string _writeFile = @"D:\\csvFileTest.csv";
+        FileStream _fsOpen, _fsCreate;
+        StreamReader _sr;
+        StreamWriter _wr2;
+
+        List<string> strList = new List<string>();
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        [DllImport("kernel32.dll")]
+        private static extern int WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetiniValues();
+        }
+        private void GetiniValues() // 초기화
+        {
+            tBoxFruit_001.Text = GetiniValue("Fruit", "001", 30, _striniPath);
+            tBoxFruit_002.Text = GetiniValue("Fruit", "002", 30, _striniPath);
+            tBoxFruit_003.Text = GetiniValue("Fruit", "003", 30, _striniPath);
+            tBoxCar_001.Text = GetiniValue("Car", "001", 30, _striniPath);
+            tBoxCar_002.Text = GetiniValue("Car", "002", 30, _striniPath);
+            tBoxCar_003.Text = GetiniValue("Car", "003", 30, _striniPath);
+        }
+        // Read
+        public string GetiniValue(string section, string key, int valueCapacity, string filePath)
+        {
+            string strReturnValue;
+            StringBuilder tmp = new StringBuilder(255);
+            int i = GetPrivateProfileString(section, key, "", tmp, 255, filePath);
+            strReturnValue = tmp.ToString();
+            return strReturnValue;
+        }
+        // Write
+        //public static void SetiniValue(string section, string key, string value, string filePath)
+        //{
+        //    WritePrivateProfileString(section, key, value, filePath);
+        //}
+        // Fruit Save Button
+        private void btnFruitSave_Click(object sender, EventArgs e)
+        {
+            WritePrivateProfileString("Fruit", "001", tBoxFruit_001.Text, _striniPath);
+            WritePrivateProfileString("Fruit", "002", tBoxFruit_002.Text, _striniPath);
+            WritePrivateProfileString("Fruit", "003", tBoxFruit_003.Text, _striniPath);
+        }
+        // Car Save Button
+        private void btnCarSave_Click(object sender, EventArgs e)
+        {
+            WritePrivateProfileString("Car", "001", tBoxCar_001.Text, _striniPath);
+            WritePrivateProfileString("Car", "002", tBoxCar_002.Text, _striniPath);
+            WritePrivateProfileString("Car", "003", tBoxCar_003.Text, _striniPath);
+        }
+
+        private void btnWriteCSV_Click(object sender, EventArgs e)
+        {
+            _fsCreate = new FileStream(_writeFile, FileMode.Create);
+            _wr2 = new StreamWriter(_fsCreate, Encoding.UTF8);
+            strList.Add("첫째줄,A,B,C,D");
+            strList.Add("둘째줄");
+            strList.Add("셋째줄");
+            for (int i = 0; i < strList.Count; i++)
+            {
+                _wr2.WriteLine(strList[i]);
+                Console.WriteLine(strList[i]);
+
+                // 중간에 제거할 목록이 있을 시 
+                //if (i != 해당 번호)
+                //{}
+            }
+
+            _wr2.WriteLine("Finish");     // 마지막 줄에 텍스트 추가
+            _wr2.Close();
+        }
+        private void btnReadCSV_Click(object sender, EventArgs e)
+        {
+            _fsOpen = new FileStream(_readFile, FileMode.OpenOrCreate);
+            _sr = new StreamReader(_fsOpen, Encoding.UTF8, false);
+            
+            while (!_sr.EndOfStream)
+            {
+                string s = _sr.ReadLine();
+                //string[] temp = s.Split(',');
+                string[] temp = s.Split(',');
+                
+                
+                strList.Add(s);
+            }
+            return;
+        }
+    }
+}
